5055c67d77e1938250d00566ee0eb7e7e0ce2c21
KNMsAnnes <annes.maria@kodingnext.com>
2026-07-02 01:14:05 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,6 @@
-print("hi")
+import streamlit as st
+
+st.title("GitHub Connection Test")
+st.success("🎉 Streamlit berhasil terhubung ke repository GitHub!")
+
+st.write("Jika halaman ini muncul, berarti deployment berhasil.")
